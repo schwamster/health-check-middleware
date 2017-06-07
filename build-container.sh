c1818d3cf86260dfc1d2b1a4c862354cdee6d9c8
Bastian Töpfer <bastian.toepfer@gmail.com>
2017-06-07 13:18:43 +0200

    upgraded to core 1.1.1

--- build-container.sh
+++ build-container.sh
@@ -1,9 +1,9 @@
 #!bin/bash
 set -e
 dotnet restore
-dotnet test test/health-check-middleware.test/project.json -xml $(pwd)/testresults/out.xml
+dotnet test test/health-check-middleware.test/health-check-middleware.test.csproj --test-adapter-path:. --logger:xunit
 rm -rf $(pwd)/package
-dotnet pack src/health-check-middleware/project.json -c release -o $(pwd)/package --version-suffix=${BuildNumber}
+dotnet pack src/health-check-middleware/health-check-middleware.csproj -c release -o $(pwd)/package --version-suffix=${BuildNumber}
 mkdir $(pwd)/symbols
 cp $(pwd)/package/*.symbols.nupkg $(pwd)/symbols
 rm $(pwd)/package/*.symbols.nupkg
